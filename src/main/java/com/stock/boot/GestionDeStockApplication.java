5a8645d534ea10ac41c4e303fb5ad753ea11d9da
aigle77 <akli.malaoui@gmail.com>
2018-05-10 21:22:51 +0200

    Ajout de depot sur git

---
+++ src/main/java/com/stock/boot/GestionDeStockApplication.java
@@ -0,0 +1,12 @@
+package com.stock.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStockApplication.class, args);
+	}
+}
